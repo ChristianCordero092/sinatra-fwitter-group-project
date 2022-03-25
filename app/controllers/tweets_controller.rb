f7d57d5023fd4b16bf720a9f07a7f53d4cb102e6
Christian <christiancordero092@gmail.com>
2022-03-25 01:45:34 -0400

    commit tree 45bf6cf6e446f76ad8cfc4b73380cdabc501cc4d

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,66 @@
 class TweetsController < ApplicationController
+    
+    get '/tweets' do
+        if logged_in?
+            @tweets = Tweet.all
+            erb :'tweets/tweets'
+        else
+            redirect to :'/login'
+        end
+    end
 
+    get '/tweets/new' do
+        if logged_in?
+            erb :'tweets/new_tweet'
+        else
+            redirect to :'/login'
+        end
+    end
 
+    post '/tweets' do
+        if logged_in?
+            if params[:content].empty?
+                redirect to :"/tweets/new"
+            else
+                @tweet = current_user.tweets.build(content: params[:content])
+                if @tweet.save
+                    redirect to :"/tweets"
+                else
+                    redirect to :"/tweets/new"
+                end
+            end
+        else
+            redirect to :'/login'
+        end
+    end
+
+    get '/tweets/:id' do 
+        if logged_in?
+            @tweet = Tweet.find_by_id(params[:id])
+            erb :'/tweets'
+        else
+            redirect to :'/login'
+        end
+    end
+
+    patch '/tweets/:id' do
+        @tweet = Tweet.find_by_id(params[:id])
+        if !params[:content].empty?
+            @tweet.update(content: params[:content])
+            @tweet.save
+            redirect to :'/tweets/#{params[:id]}'
+        else
+            redirect to :'/tweets/#{params[:id]}/edit'
+        end
+    end
+
+    post '/tweets/:id/delete' do 
+        @tweet = Tweet.find_by_id(params[:id])
+        if @tweet.user == current_user
+            @tweet.delete
+            redirect to :'/tweets'
+        else
+            redirect to :'/tweets'
+        end
+    end
 end
